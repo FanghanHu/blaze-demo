8828056526979526c1a288d774485baf2357b60e
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-04-06 20:51:10 -0500

    add task

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -4,6 +4,25 @@ import './App.html';
 
 Template.body.helpers({
     tasks() {
-        return TasksCollection.find({});
+        return TasksCollection.find({}, {sort: {createdAt: -1}});
     },
 });
+
+Template.form.events({
+    "submit .task-form"(event) {
+        event.preventDefault();
+
+        //get value from form element
+        const target = event.target;
+        const text = target.text.value;
+
+        //insert a task into the collection
+        TasksCollection.insert({
+            text,
+            createdAt: new Date()
+        });
+
+        //clear form
+        target.text.value = "";
+    }
+})
